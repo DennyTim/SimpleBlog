e82c4c29f5fc0bbff14fcd947fc41b2ba7991bc6
Tim <luckystudydanit@gmail.com>
2019-06-27 14:46:33 +0300

    Added Radium

--- src/app/components/PostsItem/PostItem.jsx
+++ src/app/components/PostsItem/PostItem.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Radium from 'radium'; // позволяет инлайн стилям добавлять псевдоклассы и псевдоэлементы
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const PostItem = ({ post, postLength = post.body.length }) => {
+  const style = {
+    boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14 )',
+    ':hover': {
+      border: '1px solid #aaa',
+      boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
+      cursor: 'pointer'
+    }
+  };
+
   return (
-    <div className='post-item'>
+    <div className='post-item' style={style}>
       <img
         className='post-item-img'
         src='//www.gravatar.com/avatar/0dcc7d49493b931c241e1433d432087a?s=200&r=pg&d=mm'
@@ -30,4 +40,4 @@ PostItem.propTypes = {
   postLength: PropTypes.number
 };
 
-export default PostItem;
+export default Radium(PostItem);
